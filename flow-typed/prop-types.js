ed1db49d5aff164f9b443d03f7a7d078589fbbaa
Miles Johnson <mileswjohnson@gmail.com>
2018-05-22 19:25:01 -0700

    Removed flow files.

--- flow-typed/prop-types.js
+++
@@ -1,5 +0,0 @@
-declare module 'prop-types' {
-  import typeof { PropTypes } from 'react';
-
-  declare export default PropTypes;
-}
--- flow-typed/react-virtualized.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-declare module 'react-virtualized/dist/commonjs/List' {
-  declare export default class List extends React.PureComponent {}
-}
