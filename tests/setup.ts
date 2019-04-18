634c99b7c6dc6af09c15b3001f3b5ee2e8dcc189
Miles Johnson <mileswjohnson@gmail.com>
2019-04-17 20:45:44 -0700

    Fix tests.

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,5 +1,9 @@
 import { loadEmojiData } from 'emojibase-test-utils';
-import EmojiDataManager from '../packages/emoji/src/EmojiDataManager';
+import { EmojiDataManager } from 'interweave-emoji';
+import EmojiDataSourceManager from '../packages/emoji/src/EmojiDataManager';
+
+const data = loadEmojiData();
 
 // Bootstrap our emoji data using the official en dataset
-EmojiDataManager.getInstance('en').parseEmojiData(loadEmojiData());
+EmojiDataSourceManager.getInstance('en').parseEmojiData(data);
+EmojiDataManager.getInstance('en').parseEmojiData(data);
