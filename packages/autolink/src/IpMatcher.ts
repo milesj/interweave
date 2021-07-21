d1861be322d59a00812d916a391740a148aeab9e
Miles Johnson <mileswjohnson@gmail.com>
2021-07-20 19:53:11 -0700

    Update TS.

--- packages/autolink/src/IpMatcher.ts
+++ packages/autolink/src/IpMatcher.ts
@@ -5,23 +5,23 @@ import { UrlMatcherOptions, UrlProps } from './types';
 import UrlMatcher, { UrlMatch } from './UrlMatcher';
 
 export default class IpMatcher extends UrlMatcher {
-  constructor(
-    name: string,
-    options?: UrlMatcherOptions,
-    factory?: React.ComponentType<UrlProps> | null,
-  ) {
-    super(
-      name,
-      {
-        ...options,
-        // IPs dont have TLDs
-        validateTLD: false,
-      },
-      factory,
-    );
-  }
+	constructor(
+		name: string,
+		options?: UrlMatcherOptions,
+		factory?: React.ComponentType<UrlProps> | null,
+	) {
+		super(
+			name,
+			{
+				...options,
+				// IPs dont have TLDs
+				validateTLD: false,
+			},
+			factory,
+		);
+	}
 
-  match(string: string): MatchResponse<UrlMatch> | null {
-    return this.doMatch(string, IP_PATTERN, this.handleMatches);
-  }
+	override match(string: string): MatchResponse<UrlMatch> | null {
+		return this.doMatch(string, IP_PATTERN, this.handleMatches);
+	}
 }
--- packages/core/src/StyleFilter.ts
+++ packages/core/src/StyleFilter.ts
@@ -4,19 +4,19 @@ import { ElementAttributes } from './types';
 const INVALID_STYLES = /(url|image|image-set)\(/i;
 
 export default class StyleFilter extends Filter {
-  attribute<K extends keyof ElementAttributes>(
-    name: K,
-    value: ElementAttributes[K],
-  ): ElementAttributes[K] {
-    if (name === 'style') {
-      Object.keys(value).forEach((key) => {
-        if (String(value[key]).match(INVALID_STYLES)) {
-          // eslint-disable-next-line no-param-reassign
-          delete value[key];
-        }
-      });
-    }
+	override attribute<K extends keyof ElementAttributes>(
+		name: K,
+		value: ElementAttributes[K],
+	): ElementAttributes[K] {
+		if (name === 'style') {
+			Object.keys(value).forEach((key) => {
+				if (String(value[key]).match(INVALID_STYLES)) {
+					// eslint-disable-next-line no-param-reassign
+					delete value[key];
+				}
+			});
+		}
 
-    return value;
-  }
+		return value;
+	}
 }
