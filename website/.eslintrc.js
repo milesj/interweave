36bac4c1f8336061c427d918aaac44b0784e64b5
Miles Johnson <mileswjohnson@gmail.com>
2022-03-04 00:03:01 -0800

    Fix lints.

---
+++ website/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	rules: {
+		'no-restricted-globals': 'off',
+		'import/no-default-export': 'off',
+		'import/no-unresolved': 'off',
+		'react/jsx-no-literals': 'off',
+		'@typescript-eslint/no-unsafe-call': 'off',
+		'@typescript-eslint/no-unsafe-member-access': 'off',
+		'@typescript-eslint/no-unsafe-assignment': 'off',
+		'@typescript-eslint/no-unsafe-argument': 'off',
+	},
+};
